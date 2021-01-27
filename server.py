902783565edb48df692d070c1da4c25c40afbc71
Sohanur Rahman <sohanur064@gmail.com>
2021-01-27 10:42:28 +0600

    add dynamic page route

--- server.py
+++ server.py
@@ -7,20 +7,8 @@ app = Flask(__name__)
 def my_home():
     return render_template('index.html')
 
-@app.route('/works')
-def works():
-    return render_template('works.html')
-
-@app.route('/about.html')
-def about():
-    return render_template('about.html')
-
-@app.route('/contact.html')
-def contact():
-    return render_template('contact.html')
-
-@app.route('/components.html')
-def components():
-    return render_template('components.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
 
